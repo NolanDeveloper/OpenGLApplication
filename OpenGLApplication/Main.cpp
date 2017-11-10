971ce404df2e1bd27e61bb789dd21655160af794
unknown <user@mil8a-118b-02.mmcs.sfedu.ru>
2017-11-10 11:01:35 +0300

    Done

--- OpenGLApplication/Main.cpp
+++ OpenGLApplication/Main.cpp
@@ -48,15 +48,32 @@ void drawWireSquare(double size) {
 static double rotateY = 0;
 static double rotateX = 0;
 
+static int primitiveNumber = 5;
+
+void drawPrimitive() {
+	switch (primitiveNumber % 9) {
+	case 0: glutWireTeapot(1.2); break;
+	case 1: glutWireSphere(1, 20, 20); break;
+	case 2: glutWireCube(1); break;
+	case 3: glutWireCone(1, 1, 20, 20); break;
+	case 4: glutWireTorus(0.8, 1, 20, 20); break;
+	case 5: glutWireDodecahedron(); break;
+	case 6: glutWireOctahedron(); break;
+	case 7: glutWireTetrahedron(); break;
+	case 8: glutWireIcosahedron(); break;
+	}
+}
+
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
-	
+
 	glLoadIdentity();
 	glTranslatef(-0.5, 0.5, 0);
 	glRotatef(180. / 2 * rotateY, 0, 1, 0);
 	glRotatef(180. / 2 * rotateX, 1, 0, 0);
-	glutWireTeapot(0.3);
+	glScalef(0.2, 0.2, 0.2);
+	drawPrimitive();
 
 	glLoadIdentity();
 	glTranslatef(0.5, 0.5, 0);
@@ -73,9 +90,19 @@ void render() {
 	glutSwapBuffers();
 }
 
-void motion(int x, int y) {
-	rotateY = 2. * x / window.width - 1;
-	rotateX = 2. * y / window.height - 1;
+void mouse(int button, int state, int x, int y) {
+	if (GLUT_DOWN != state) return;
+	++primitiveNumber;
+	glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'w': rotateX -= 0.1; break;
+	case 's': rotateX += 0.1; break;
+	case 'a': rotateY += 0.1; break;
+	case 'd': rotateY -= 0.1; break;
+	}
 	glutPostRedisplay();
 }
 
@@ -91,8 +118,9 @@ int main(int argc, char ** argv) {
 	glutInitWindowSize(500, 500);
 	glutCreateWindow("Title goes here");
 	glutDisplayFunc(render);
-	glutMotionFunc(motion);
 	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
 
 	glutMainLoop();
 	return 0;
